c3478f853c6ce2c3033ac43b35465f934e6aefb4
ManishRacha12 <rachamanish12@gmail.com>
2023-10-20 12:22:47 -0500

    display second lowest details

---
+++ nestedlist.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    stu=[]
+    for i in range(int(input())):
+        name = input()
+        score = float(input())
+        stu.append([name,score])
+    print(stu)
+
+
