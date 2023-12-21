22e7655af4b255ecd72799686c7f66698d9e25a0
ManishRacha12 <rachamanish12@gmail.com>
2023-12-20 20:23:24 -0600

    find unique element in list

---
+++ lonely_int.py
@@ -0,0 +1,18 @@
+
+def lonelyinteger(a):
+    for i in a:
+        if a.count(i)<2:
+            print(i)
+            
+            
+    
+        
+    
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    a = list(map(int, input().rstrip().split()))
+    result = lonelyinteger(a)
+
