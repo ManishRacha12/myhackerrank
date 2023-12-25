d882bd060cacb6f10df6eaafbdecd9fe4dd43978
ManishRacha12 <rachamanish12@gmail.com>
2023-12-24 23:39:48 -0600

    changes made

--- counter_collection.py
+++ counter_collection.py
@@ -1,5 +1,5 @@
 '''
- is a shoe shop owner. His shop has  number of shoes.
+is a shoe shop owner. His shop has  number of shoes.
 He has a list containing the size of each shoe he has in his shop.
 There are  number of customers who are willing to pay  amount of money only if they get the shoe of their desired size.
 
@@ -15,16 +15,17 @@ The next  lines contain the space separated values of the  desired by the custom
 Constraints
 '''
 
-
 from collections import Counter
-X = int(input())
-N = map(int,input().split())
-x = int(input())
-L = map(tuple,(map(int,input().split()) for _ in range(x)))
-n = Counter(N)
-p =0
-for i in L:
-    if i[0] in n.keys() and n[i[0]] >0 :
-        n[i[0]] = n[i[0]]-1
-        p = p+i[1]
-print(p)
+X=int(input())      #No of shoes
+X_list=map(int,input().split())     #The second line contains the space separated list of all the shoe sizes in the shop.
+N=int(input())          #The third line contains N, the number of customers.
+N_xi=map(tuple,(map(int,input().split()) for _ in range(N)))    #The next  lines contain the space separated values of the  desired by the customer and , the price of the shoe.
+shoe_list=Counter(X_list)
+sum=0
+for each_item in N_xi:
+    if each_item[0] in shoe_list.keys() and shoe_list[each_item[0]]>0:
+        shoe_list[each_item[0]]=shoe_list[each_item[0]]-1
+        sum=sum+each_item[1]
+print(sum)
+        
+
