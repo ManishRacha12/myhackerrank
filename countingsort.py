d128f80900304a46475e62452d3c1cb81bcb8fb2
ManishRacha12 <rachamanish12@gmail.com>
2023-12-25 20:05:29 -0600

    changes made

--- countingsort.py
+++ countingsort.py
@@ -1,22 +1,4 @@
-def count_occurrences(arr):
-    # Initialize an empty dictionary to store the counts
-    count_dict = {}
-
-    # Count the occurrences of each value in the input array
-    for num in arr:
-        count_dict[num] = count_dict.get(num, 0) + 1
-
-    # Convert the dictionary to a list of tuples (value, count)
-    result = [(key, value) for key, value in count_dict.items()]
-
-    return result
-
-# Example input
-input_numbers = [1, 2, 2, 3, 4, 4, 5, 5, 5, 5]
-
-# Count occurrences
-occurrences = count_occurrences(input_numbers)
-
-# Print the result
-for value, count in occurrences:
-    print(f"{value}: {count} times")
+n=int(input())
+arr=list(map(int,input().split()))
+visited = [False for i in range(n)]
+print(visited)
