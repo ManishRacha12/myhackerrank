c835356af3097fb823a30265ea7004afbce61453
ManishRacha12 <rachamanish12@gmail.com>
2023-12-29 11:27:04 -0600

    changes made

--- diagonal_diff.py
+++ diagonal_diff.py
@@ -4,3 +4,8 @@ for i in range(n):
     i1=list(map(int,input().split()))
     arr.append(i1)
 print(arr)
+rows=len(arr)
+cols=len(arr[0])
+frt_diagonal=sum(arr[i][i] for i in range(min(rows,cols)))
+sec_diagonal=sum(arr[i][cols-i-1] for i in range(min(rows,cols)))
+print(abs(frt_diagonal-sec_diagonal))
