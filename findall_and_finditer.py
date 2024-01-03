5dffe6b1f049cbb55c16beb55c13f94507ff88bd
ManishRacha12 <rachamanish12@gmail.com>
2024-01-02 22:50:00 -0600

    changes made

--- findall_and_finditer.py
+++ findall_and_finditer.py
@@ -1,6 +1,6 @@
 import re
 
-pattern = r'[^aeiouAEIOU\s]*([\+\-aeiouAEIOU]{2,})[^aeiouAEIOU\s]'
+pattern = r'[^aeiouAEIOU\s\+\-]*([aeiouAEIOU]{2,})[^aeiouAEIOU\s\+\-]'   #first not take vowels,special characters, spaces, second takes atleast two vowels, third same as first 
 string=input()
 match1=re.findall(pattern,string)
 if match1:
