b679b374cbaaa837294f4bc9a13f20162d672266
ManishRacha12 <rachamanish12@gmail.com>
2024-01-03 22:49:34 -0600

    changes made

--- design_doormat.py
+++ design_doormat.py
@@ -3,19 +3,16 @@
 Mat size must be X. ( is an odd natural number, and  is  times .)
 The design should have 'WELCOME' written in the center.
 The design pattern should only use |, . and - characters."""
-def print_door_mat(rows, columns):
-    for i in range(1, rows, 2):
-        pattern = (".|." * i).center(columns, '-')
+def doormat(rows,columns):
+    for i in range(1,rows,2):
+        pattern=(".|."*i).center(columns,'-')   #using center method padding to center based on columns with '-' and using pattern '.|.'
         print(pattern)
-
-    welcome_line = 'WELCOME'.center(columns, '-')
+    welcome_line='WELCOME'.center(columns,'-')
     print(welcome_line)
-
-    for i in range(rows - 2, 0, -2):
-        pattern = (".|." * i).center(columns, '-')
+    for i in range(rows-2,0,-2):
+        pattern=(".|."*i).center(columns,'-')
         print(pattern)
 
-# Example usage with mat size 7 (you can change this value)
-mat_size = int(input())
-mat_width = mat_size * 3
-print_door_mat(mat_size, mat_width)
+
+N,M=map(int,input().split())
+doormat(N,M)
